f7af594a18320eec24868f7cf7e09feee8e2987b
Othman Luqman <othman.luqman.hazim@gmail.com>
2021-11-04 15:47:24 +0300

    Update task using a latest DB.

--- app/Http/Controllers/CatagoryController.php
+++ app/Http/Controllers/CatagoryController.php
@@ -10,66 +10,63 @@ class CatagoryController extends Controller
     public function getAllCatagorys()
     {
         $start = microtime(true);
-        $roster = array();
-        $catagorys = DB::select('select * from catagory where parent_id = ?',[0]);
-        foreach ($catagorys as $catagory) {
-            $childs = DB::select('select * from catagory where parent_id = ?', [$catagory->id]);
-            if (count($childs) > 0) {
-                foreach ($childs as $child) {
-                    $subchilds = DB::select('select * from catagory where parent_id = ?', [$child->id]);
-                    if (count($subchilds) > 0) {
-                        foreach ($subchilds as $subchild) {
-                            $roster[$catagory->name][$child->name][$subchild->id] = $subchild->name;
-                        }
-                    }else{
-                        $roster[$catagory->name][$child->name] = [];
-                    }
-                }
+
+        $categories = DB::select('select id,name,parent_id from categories');
+        $categories = collect($categories)->map(function($x){ return (array) $x; })->toArray();
+
+        $childs = array();
+        foreach($categories as &$item){
+            $childs[$item['parent_id']][] = &$item;
+            unset($item);
+        }
+        foreach($categories as &$item){
+            if (isset($childs[$item['id']])){
+                $item['childs'] = $childs[$item['id']];
+            }else{
+                $item['childs'] = [];
             }
         }
-        return $roster;
-        $time = microtime(true) - $start; //Used for Efficiency measurement
-        //return [$roster,$time];
+        return $childs[0];
+        // $time = microtime(true) - $start;
+        //return [$childs[0],$time];
     }
     
     public function getCatagoryById($id)
     {
-        $catagorys = DB::select('select * from catagory where id = ?',[$id]);
-        foreach ($catagorys as $catagory) {
-            $childs = DB::select('select * from catagory where parent_id = ?', [$catagory->id]);
-            if (count($childs) > 0) {
-                foreach ($childs as $child) {
-                    $subchilds = DB::select('select * from catagory where parent_id = ?', [$child->id]);
-                    if (count($subchilds) > 0) {
-                        foreach ($subchilds as $subchild) {
-                            $roster[$catagory->name][$child->name][$subchild->id] = $subchild->name;
-                        }
-                    }else{
-                        $roster[$catagory->name][$child->name] = [];
-                    }
-                }
+        $categories = DB::select('select id,name,parent_id from categories');
+        $categories = collect($categories)->map(function($x){ return (array) $x; })->toArray();
+
+        $childs = array();
+        foreach($categories as &$item){
+            $childs[$item['parent_id']][] = &$item;
+            unset($item);
+        }
+        foreach($categories as &$item){
+            if (isset($childs[$item['id']])){
+                    $item['childs'] = $childs[$item['id']];
             }
         }
-        return $roster;
+        return $childs[$id];
     }
+
     public function getRecursiveCatagoryById($id)
     {
-        $subchilds = DB::select('select * from catagory where id = ?',[$id]);
-        foreach ($subchilds as $subchild) {
-            $childs = DB::select('select * from catagory where id = ?', [$subchild->parent_id]);
-            if (count($childs) > 0) {
-                foreach ($childs as $child) {
-                    $catagorys = DB::select('select * from catagory where id = ?', [$child->parent_id]);
-                    if (count($catagorys) > 0) {
-                        foreach ($catagorys as $catagory) {
-                            $roster[$subchild->name][$child->name][$catagory->name] = [];
-                        }
-                    }else{
-                        $roster[$child->name][$catagory->name] = [];
-                    }
-                }
+
+        $categories = DB::select('select id,name,parent_id from categories');
+        $categories = collect($categories)->map(function($x){ return (array) $x; })->toArray();
+
+        $childs = array();
+        foreach($categories as &$item){
+            $childs[$item['id']][] = &$item;
+            unset($item);
+        }
+        foreach($categories as &$item){
+            if (isset($childs[$item['parent_id']])){
+                $item['parent'] = $childs[$item['parent_id']];
+            }else{
+                $item['parent'] = [];
             }
         }
-        return $roster;
+        return $childs[$id];
     }
 }
